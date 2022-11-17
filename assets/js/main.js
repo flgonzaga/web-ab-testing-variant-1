a129159b73726b3992f5b8222b88505c502e451b
Fabio Gonzaga <fabiogonzaga@pecege.com>
2022-11-17 11:48:27 -0300

    fix: added regex

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,7 @@ const nextTitle = 'Home Page - Variant 1';
 const nextState = { additionalInformation: 'Updated the URL with JS' };
 
 // This will create a new entry in the browser's history, without reloading
-window.history.pushState(nextState, nextTitle, nextURL);
+// window.history.pushState(nextState, nextTitle, nextURL);
 
 // This will replace the current entry in the browser's history, without reloading
 window.history.replaceState(nextState, nextTitle, nextURL);
