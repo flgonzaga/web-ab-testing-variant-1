ac6d1279b66d93543bc080345a43f1dabee35cba
Fabio Gonzaga <fabiogonzaga@pecege.com>
2022-11-17 13:55:11 -0300

    fix: added regex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,22 @@
-// Strings with some data
-const domainStr = 'flgonzaga.github.io/web-ab-testing-variant-1'
-const newDomain = 'flgonzaga.github.io/web-ab-testing-original'
+// const regex = '/variant-1/gm';
 
-// Make sure your string is regex friendly
-// This will replace dots for '\'.
-const regexUrl = /\./gm;    
-const substr = `\\\.`;
-const domain = domainStr.replace(regexUrl, substr);
-// domain is a regex friendly string: 'my-domain\.com'
-console.log('Regex expresion for domain', domain)
+// Alternative syntax using RegExp constructor
+const regex = new RegExp('variant-1', 'gm')
 
-// HERE!!! You can 'assemble a complex regex using string pieces.
-const re = new RegExp( String.raw `([\'|\"]https:\/\/)(${domain})(\S+[\'|\"])`, 'gm');
+const str = window.location.href;
+const subst = `orginal`;
 
-// now I'll use the regex expression groups to replace the domain
-const domainSubst = `$1${newDomain}$3`;
-console.log(domainSubst)
-// const page contains all the html text
-// const result = page.replace(re, domainSubst);
+// The substituted value will be contained in the result variable
+const result = str.replace(regex, subst);
+console.log('Substitution result: ', result);
 
 // Current URL: https://my-website.com/page_a
-// const nextURL = 'https://flgonzaga.github.io/web-ab-testing-original/';
+const nextURL = 'https://flgonzaga.github.io/web-ab-testing-original/';
 const nextTitle = 'Home Page - Variant 1';
 const nextState = { additionalInformation: 'Updated the URL with JS' };
 
 // This will create a new entry in the browser's history, without reloading
-window.history.pushState(nextState, nextTitle, domainSubst);
+window.history.pushState(nextState, nextTitle, result);
 
 // This will replace the current entry in the browser's history, without reloading
-window.history.replaceState(nextState, nextTitle, domainSubst);
+window.history.replaceState(nextState, nextTitle, result);
