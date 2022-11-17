43b0eb42c19a268669d2977f26718d82aec2bc46
Fabio Gonzaga <fabiogonzaga@pecege.com>
2022-11-17 11:45:26 -0300

    fix: added regex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,26 @@
+// Strings with some data
+const domainStr = 'flgonzaga.github.io/web-ab-testing-variant-1'
+const nextURL = 'flgonzaga.github.io/web-ab-testing-original'
+
+// Make sure your string is regex friendly
+// This will replace dots for '\'.
+const regexUrl = /\./gm;    
+const substr = `\\\.`;
+const domain = domainStr.replace(regexUrl, substr);
+// domain is a regex friendly string: 'my-domain\.com'
+console.log('Regex expresion for domain', domain)
+
+// HERE!!! You can 'assemble a complex regex using string pieces.
+const re = new RegExp( String.raw `([\'|\"]https:\/\/)(${domain})(\S+[\'|\"])`, 'gm');
+
+// now I'll use the regex expression groups to replace the domain
+const domainSubst = `$1${nextURL}$3`;
+
+// const page contains all the html text
+//onst result = page.replace(re, domainSubst);
+
 // Current URL: https://my-website.com/page_a
-const nextURL = 'https://flgonzaga.github.io/web-ab-testing-original/';
+// const nextURL = 'https://flgonzaga.github.io/web-ab-testing-original/';
 const nextTitle = 'Home Page - Variant 1';
 const nextState = { additionalInformation: 'Updated the URL with JS' };
 
