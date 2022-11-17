d5239f2ea9d6e8ad6127ecef8a44b31d74a91e92
Fabio Gonzaga <fabiogonzaga@pecege.com>
2022-11-17 15:42:41 -0300

    fix: added regex

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
 // Alternative syntax using RegExp constructor
 const regex = new RegExp('variant-1', 'gm')
 
-const str = window.location.href;
+const str = 'https://flgonzaga.github.io/web-ab-testing-original/';
 const subst = `original`;
 
 // The substituted value will be contained in the result variable
@@ -16,7 +16,7 @@ const nextTitle = 'Home Page - Variant 1';
 const nextState = { additionalInformation: 'Updated the URL with JS' };
 
 // This will create a new entry in the browser's history, without reloading
-// window.history.pushState(nextState, nextTitle, result);
+window.history.pushState(nextState, nextTitle, result);
 
 // This will replace the current entry in the browser's history, without reloading
 window.history.replaceState(nextState, nextTitle, result);
