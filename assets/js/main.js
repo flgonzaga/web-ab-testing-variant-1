9658075d2e0f8f0cc90e7f2bda15897783b6105e
Fabio Gonzaga <fabiogonzaga@pecege.com>
2022-11-17 11:53:12 -0300

    fix: added regex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 // Strings with some data
 const domainStr = 'flgonzaga.github.io/web-ab-testing-variant-1'
-const nextURL = 'flgonzaga.github.io/web-ab-testing-original'
+const newDomain = 'flgonzaga.github.io/web-ab-testing-original'
 
 // Make sure your string is regex friendly
 // This will replace dots for '\'.
@@ -14,10 +14,10 @@ console.log('Regex expresion for domain', domain)
 const re = new RegExp( String.raw `([\'|\"]https:\/\/)(${domain})(\S+[\'|\"])`, 'gm');
 
 // now I'll use the regex expression groups to replace the domain
-const domainSubst = `$1${nextURL}$3`;
+const domainSubst = `$1${newDomain}$3`;
 
 // const page contains all the html text
-//onst result = page.replace(re, domainSubst);
+const result = page.replace(re, domainSubst);
 
 // Current URL: https://my-website.com/page_a
 // const nextURL = 'https://flgonzaga.github.io/web-ab-testing-original/';
@@ -25,7 +25,7 @@ const nextTitle = 'Home Page - Variant 1';
 const nextState = { additionalInformation: 'Updated the URL with JS' };
 
 // This will create a new entry in the browser's history, without reloading
-// window.history.pushState(nextState, nextTitle, nextURL);
+window.history.pushState(nextState, nextTitle, domainSubst);
 
 // This will replace the current entry in the browser's history, without reloading
-window.history.replaceState(nextState, nextTitle, nextURL);
+window.history.replaceState(nextState, nextTitle, domainSubst);
